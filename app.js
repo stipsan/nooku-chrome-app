cfdb4687170818ea4a395b6fe2413d55c5dc7669
Stian Didriksen <stipsan@me.com>
2014-01-02 16:39:09 +0100

    removing analytics code and CSP

--- app.js
+++ app.js
@@ -73,85 +73,15 @@ head.js(
     }
 );
 
-/* Analytics code here, needs to be loaded separately from the rest in case the app is offline */
-var Analytics;
-head.js('http://api.mixpanel.com/site_media/js/api/mixpanel.js', function(){
-    try {
-        Analytics = new MixpanelLib('6905aafac333e44027e556de8c8f1076', 'Analytics');
-    } catch(err) {
-        ['track', 'track_funnel', 'register', 'register_once', 'identify'].forEach(function(key){
-            Analytics[key] = function(){};
-        });
-    }
-
-    if(window.parent == window && !sessionStorage['initialized']) {
-        Analytics.track('Initialized', {
-            'Application User Agent': window.navigator.userAgent,
-            'CPU Cores': Titanium.Platform.getProcessorCount(),
-            'Language': window.navigator.language,
-            'Nooku Desktop Version': Titanium.API.Application.getVersion(),
-            'Platform': Titanium.getPlatform().toUpperCase() + ' ' + Titanium.Platform.getVersion(),
-            'WebKit': navigator.userAgent.replace(/.*AppleWebKit\/([\d\.]+).*/, "$1")
-        }, function(){
-            console.log('Application Launch Tracked');
-            console.dir(Analytics);
-        });
-
-        var win = Titanium.UI.getCurrentWindow();
-        if(!localStorage['window.width']) localStorage['window.width'] = window.innerWidth;
-        if(!localStorage['window.height']) localStorage['window.height'] = window.innerHeight;
-        if(!localStorage['window.left']) localStorage['window.left'] = win.getX();
-        if(!localStorage['window.top']) localStorage['window.top'] = win.getY();
-
-        if(localStorage['window.maximized'] && localStorage['window.maximized'] == 'true') {
-            var win = Titanium.UI.getCurrentWindow();
-            win.maximize();
-        } else {
-            win.width = parseInt(localStorage['window.width']);
-            win.height = parseInt(localStorage['window.height']);
-            win.setX(parseInt(localStorage['window.left']));
-            win.setY(parseInt(localStorage['window.top']));
-            //window.resizeTo(localStorage['window.width'], localStorage['window.height']);
-            //window.moveTo(localStorage['window.left'], localStorage['window.top']);
-        }
-
-        Titanium.addEventListener(Titanium.MAXIMIZED, function(){
-            var win = Titanium.UI.getCurrentWindow();
-            localStorage['window.maximized'] = win.isMaximized();
-        });
-        Titanium.addEventListener(Titanium.MOVED, function(){
-            var win = Titanium.UI.getCurrentWindow(), x = win.getX(), y = win.getY();
-            if(win.isMaximized()) return;
-            localStorage['window.left'] = win.getX();
-            localStorage['window.top'] = win.getY();
-            console.dir({x: x, y: y});
-            //localStorage['window.left'] = window.screenX;
-            //localStorage['window.top'] = window.screenY;
-            localStorage['window.maximized'] = false;
-        });
-        window.addEventListener('resize', function(event){
-            var win = Titanium.UI.getCurrentWindow();
-            if(win.isMaximized()) return;
-            localStorage['window.width'] = Math.max(640, win.getWidth());
-            localStorage['window.height'] = Math.max(480, win.getHeight());
-            //localStorage['window.width'] = Math.max(640, window.getWidth());
-            //localStorage['window.height'] = Math.max(480, window.getHeight());
-            localStorage['window.maximized'] = false;
-        });
-
-        //Prevent this from running more than once
-        sessionStorage['initialized'] = true;
-    }
-});
-
 (function(){
+    /** @TODO experimental window drag support on in-frame UI elements (like the bottom statusbar)
     var listening = false, prevEvent, drag = function(event){
         if(!prevEvent) prevEvent = event;
         var win = Titanium.UI.getCurrentWindow();
-        /*
-         win.setX(win.getX() + (event.screenX - prevEvent.screenX));
-         win.setY(win.getY() + (event.screenY - prevEvent.screenY));
-         //*/
+        
+         //win.setX(win.getX() + (event.screenX - prevEvent.screenX));
+         //win.setY(win.getY() + (event.screenY - prevEvent.screenY));
+
         window.moveBy(event.screenX - prevEvent.screenX, event.screenY - prevEvent.screenY);
         prevEvent = event;
     }, detach = function(){
@@ -184,4 +114,5 @@ head.js('http://api.mixpanel.com/site_media/js/api/mixpanel.js', function(){
         var list = document.getElement('.front ul');
         if(list) list.addEventListener('scroll', detach);
     }, 10000);
+   */
 })();
