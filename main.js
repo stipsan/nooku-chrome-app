3e2dbda1f5573ac980e0c9e4e72ce08b91dbd68d
Stian Didriksen <stipsan@me.com>
2014-01-03 01:00:12 +0100

    switching to AngularJS

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-/**
- * Listens for the app launching then creates the window
- *
- * @see http://developer.chrome.com/trunk/apps/app.runtime.html
- * @see http://developer.chrome.com/trunk/apps/app.window.html
- */
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create('main.html', {
     'width': 1024,
     'height': 768
   });
